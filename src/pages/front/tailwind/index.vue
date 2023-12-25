0d3aa7ffe0edbef5174f097c4bc704d35fec6f56
hasdsde <1259305706@qq.com>
2023-12-26 01:12:21 +0800

    代码生成器有思路了

--- src/pages/front/tailwind/index.vue
+++ src/pages/front/tailwind/index.vue
@@ -1,7 +1,69 @@
 <template>
-  <div>
-    tailwind
+  <div class="q-pa-sm flex">
+    <q-card class="w-1/5 h-full">
+      <div class="q-pa-md">
+        <q-btn-toggle
+            v-model="leftTab"
+            spread
+            class="my-custom-toggle"
+            no-caps
+            unelevated
+            toggle-color="primary"
+            color="white"
+            text-color="primary"
+            :options="[
+          {label: '模板', value: 'template'},
+          {label: '变量', value: 'vars'},
+          {label: '函数', value: 'fun'},
+          {label: '引入', value: 'import'},
+        ]"
+        />
+        <q-tab-panels v-model="leftTab" animated>
+          <q-tab-panel name="template">
+            <div class="text-h6">Mails</div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </q-tab-panel>
+
+          <q-tab-panel name="vars">
+            <div class="text-h6">Alarms</div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </q-tab-panel>
+
+          <q-tab-panel name="fun">
+            <div class="text-h6">Movies</div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </q-tab-panel>
+
+          <q-tab-panel name="import">
+            <div class="text-h6">Movies</div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </q-tab-panel>
+        </q-tab-panels>
+      </div>
+    </q-card>
+    <q-card class="w-3/5 h-full">
+      <q-card-section>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat.
+      </q-card-section>
+    </q-card>
+    <q-card class="w-3/5 h-full">
+      <q-card-section>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat.
+      </q-card-section>
+    </q-card>
+
   </div>
 </template>
 <script lang="ts" setup>
+import {ref} from "vue";
+
+const leftTab = ref('template')
 </script>
+<style lang="sass" scoped>
+.my-custom-toggle
+  border: 1px solid #027be3
+</style>
